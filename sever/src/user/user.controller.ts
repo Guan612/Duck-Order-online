0416bb8c59ad300df72b3387a9fc436b26dacb5f
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-05 18:22:20 +0800

    用户模块

---
+++ sever/src/user/user.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+@ApiTags('users')
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @ApiOperation({ summary: '创建用户' })
+  create(@Body() createUserDto: User) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: '查找所有用户' })
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: '查找指定用户' })
+  @ApiParam({ name: 'id', type: 'number' }) // 添加参数说明
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: '更新用户信息' })
+  @ApiParam({ name: 'id', type: 'number' })
+  update(@Param('id') id: string, @Body() updateUserData) {
+    //console.log(updateUserData);
+    return this.userService.update(+id, updateUserData);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除用户' })
+  @ApiParam({ name: 'id', type: 'number' })
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
