4b328e9ab39bf2a801b2fda40d78c7bf6b23705e
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-06 10:25:32 +0800

    首页

---
+++ front/admin/src/pages/welcome/index.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-4xl font-bold">欢迎来到鸭鸭在线点餐管理</h1>
+			<p className="text-lg mt-4">管理面板</p>
+		</div>
+	);
+}
