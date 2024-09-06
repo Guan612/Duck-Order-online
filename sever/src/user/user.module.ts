c8ded4cfda3046e506043137488714834e0229d7
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-06 15:47:43 +0800

    添加注册中间件

--- sever/src/user/user.module.ts
+++ sever/src/user/user.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CheckUserExistsMiddleware } from './middleware/check-user-exists.middleware';
+import { HashPasswordMiddleware } from './middleware/hash-password.middleware';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],//注意需要把prismaService也注入进来
+  providers: [UserService, PrismaService], //注意需要把prismaService也注入进来
 })
-export class UserModule {}
+export class UserModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CheckUserExistsMiddleware, HashPasswordMiddleware)
+      .forRoutes({ path: '/user/register', method: RequestMethod.POST })
+  }
+}
