7c23c098940c7bd83225875ab38e80c266ab31b2
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-11 15:36:51 +0800

    Update jwt.strategy.ts

--- sever/src/user/strategies/jwt.strategy.ts
+++ sever/src/user/strategies/jwt.strategy.ts
@@ -9,7 +9,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: configService.get('JWT_SECRET') || 'yayaPassword',
+      signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') || '12h' },
     });
   }
 
