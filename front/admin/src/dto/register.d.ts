51bf4c86bc8da1d764a3afc67fc211c73d738cfc
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-11 11:07:54 +0800

    注册类型检查

---
+++ front/admin/src/dto/register.d.ts
@@ -0,0 +1,12 @@
+interface RegisterDto {
+	loginId: string;
+	nickname: string;
+	password: string;
+	email: string;
+	phone: string;
+	address: string;
+	cos: number;
+	accept: boolean;
+}
+
+export default RegisterDto;
