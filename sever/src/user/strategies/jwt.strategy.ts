ed2732872bcbcbe19252e7241cc2424d2cd39e82
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-12 15:22:41 +0800

    调整palyload数据

--- sever/src/user/strategies/jwt.strategy.ts
+++ sever/src/user/strategies/jwt.strategy.ts
@@ -15,6 +15,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return {  username: payload.username };
+    return {  loginId: payload.loginId, userId: payload.userId, cost: payload.cost };
   }
 }
