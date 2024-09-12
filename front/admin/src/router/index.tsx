9cb436d02758acc94af853b7e30b9d0565eb870b
Guan612 <tiantony53@163.com>
2024-09-12 09:41:36 +0800

    修改前端鉴权守卫

--- front/admin/src/router/index.tsx
+++ front/admin/src/router/index.tsx
@@ -5,37 +5,43 @@ import UserAdmin from "../pages/useradmin";
 import Welcome from "../pages/welcome";
 import Login from "../pages/login";
 import Register from "../pages/register";
+import useAuth from "../hooks/login/useAuth";
+
+const RequireAuth = ({ element }) => {
+    const auth = useAuth();
+    return auth ? element : <Navigate to="/auth" />;
+};
 
 const routes = [
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				path: "/",
-				element: <Welcome />,
-			},
-			{
-				path: "/useradmin",
-				element: <UserAdmin />,
-			},
-		],
-	},
-	{
-		path: "/auth",
-		children: [
-			{
-				path: "/auth",
-				element: <Login />,
-			},
-			{
-				path: "/auth/register",
-				element: <Register />,
-			},
-		],
-	},
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <RequireAuth element={<Welcome />} />,
+            },
+            {
+                path: "/useradmin",
+                element: <RequireAuth element={<UserAdmin />} />,
+            },
+        ],
+    },
+    {
+        path: "/auth",
+        children: [
+            {
+                path: "/auth",
+                element: <Login />,
+            },
+            {
+                path: "/auth/register",
+                element: <Register />,
+            },
+        ],
+    },
 ];
 
 export default function Router() {
-	return useRoutes(routes);
+    return useRoutes(routes);
 }
