ced1d6930c4702c980120f5af2369360ed7b2e68
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-13 11:53:08 +0800

    实时刷新用户状态

--- front/admin/src/hooks/login/useAuth.tsx
+++ front/admin/src/hooks/login/useAuth.tsx
@@ -1,9 +1,8 @@
 import userStore from "../../store/userstore";
 
-const userInfo = userStore.getState().userInfo;
-
 const useAuth = () => {
-    return userInfo.token;
-}
+	const userInfo = userStore((state) => state.userInfo); // 订阅 userInfo 的状态变化
+	return userInfo?.token ? true : false; // 如果 token 存在，表示用户已登录
+};
 
 export default useAuth; // 导出useAuth函数
