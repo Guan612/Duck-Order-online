519834144f7840a2d0eee4697625575371c164fd
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-13 18:29:40 +0800

    Update app.module.ts

--- sever/src/app.module.ts
+++ sever/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, OrderModule, MenuModule],
   controllers: [],
   providers: [],
 })
