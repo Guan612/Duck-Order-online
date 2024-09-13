b9e602919374890f319ddbddbaf8bce10805c4bb
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-13 11:51:29 +0800

    Update App.tsx

--- front/admin/src/App.tsx
+++ front/admin/src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
 function App() {
 	return (
-		<BrowserRouter>
-			<Router />
-		</BrowserRouter>
+		<>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</>
 	);
 }
 
