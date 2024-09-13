f2e0744c05a03afb871fbc1a577ce28841de4a57
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-13 11:53:20 +0800

    添加了验证未注册用户逻辑

--- sever/src/user/pipe/check-user-login-role.pipe.ts
+++ sever/src/user/pipe/check-user-login-role.pipe.ts
@@ -7,6 +7,9 @@ export class CheckUserLoginRolePipe implements PipeTransform {
   async transform(value: any) {
     const { loginId } = value;
     const existingUser = await this.userService.findByLoginId(loginId);
+    if (!existingUser) {
+      throw new HttpException('用户不存在', 403);
+    }
     if (existingUser.cost !== 2 && existingUser.cost !== 1) {
       throw new HttpException('请使用管理员账户或服务员账户登录该后台', 403);
     } else {
