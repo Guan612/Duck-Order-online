19a62a4606ffdc3f56dba08c0d2ba5cef1f58f6c
Guan612 <tiantony53@163.com>
2024-09-15 08:04:57 +0800

    添加socketio

--- sever/src/app.module.ts
+++ sever/src/app.module.ts
@@ -4,10 +4,11 @@ import { OrderModule } from './order/order.module';
 import { MenuModule } from './menu/menu.module';
 import { UploadModule } from './upload/upload.module';
 import { CartModule } from './cart/cart.module';
+import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [UserModule, OrderModule, MenuModule, UploadModule, CartModule],
   controllers: [],
-  providers: [],
+  providers: [ChatGateway],
 })
 export class AppModule {}
---
+++ sever/src/chat/chat.gateway.ts
@@ -0,0 +1,23 @@
+import { UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server, Socket } from 'socket.io';
+import { JwtAuthGuard } from 'src/user/guards/jwt-user.guard';
+
+@WebSocketGateway(3001, { namespace: '/chat', cors: { origin: '*' } })
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('chat')
+  //@UseGuards(JwtAuthGuard)
+  handleEvent(@MessageBody() payload: string, client: Socket) {
+    this.server.emit('chat', payload); // 只回复发送消息的客户端
+  }
+}
