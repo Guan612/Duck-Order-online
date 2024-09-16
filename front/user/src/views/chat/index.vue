0042e3c7574b465e27f489584e27c1ee2702023f
Guan612 <tiantony53@163.com>
2024-09-16 13:01:05 +0800

    添加chatid

--- front/user/src/views/chat/index.vue
+++ front/user/src/views/chat/index.vue
@@ -3,31 +3,38 @@ import { ref } from 'vue';
 import { io } from "socket.io-client";
 
 const socket = io('http://localhost:3001/chat');
-const msg = ref('');
-const messages = ref([]);
+const msg = ref({
+    clientId: '',
+    message: ''
+});
+const messages = ref([]);  // 存储所有消息
+const clientId:string = ref('');   // 当前客户端 ID
+
 socket.on('connect', () => {
     console.log('Connected to WebSocket server');
+    clientId.value = socket.id;  // 获取客户端 ID
 });
 
 socket.on('chat', (msg) => {
     console.log('Message from server:', msg);
-    messages.value.push(msg);
+    messages.value.push(msg);  // 存储接收到的消息
 });
 
 const sendMessage = () => {
-    if (msg.value.trim() !== '') {
-        socket.emit('chat', msg.value); // 发送输入框中的消息
-        console.log('Message sent:', msg.value);
-        msg.value = ''; // 清空输入框
+    if (msg.value.message.trim() !== '') {
+        socket.emit('chat', {
+            clientId: clientId.value,  // 发送当前客户端 ID
+            message: msg.value.message // 发送消息
+        });
+        console.log('Message sent:', clientId);
+        msg.value.message = '';  // 清空输入框
     }
-}
-
+};
 </script>
-
 <template>
-    <input type="text" v-model="msg" />
+    <input type="text" v-model="msg.message" />
     <button @click="sendMessage">Send</button>
-    <div class="" v-for="message in messages">
+    <div :class="msg.clientId === clientId ? 'bg-transblue' : 'bg-transpink'" v-for="message in messages">
         {{ message }}
     </div>
 </template>
