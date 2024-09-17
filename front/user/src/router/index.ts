be545a72790d0761e156d965daedc8da78f3204e
Guan612 <tiantony53@163.com>
2024-09-17 10:44:57 +0800

    添加路由鉴权守卫

--- front/user/src/router/index.ts
+++ front/user/src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "@/stores/userStore";
+
 import Layout from "@/views/layout/index.vue";
 import Chat from "@/views/chat/index.vue";
 import Delivery from "@/views/delivery/index.vue";
 import Home from "@/views/home/index.vue";
 import Chart from "@/views/chart/index.vue";
 import Me from "@/views/me/index.vue";
+import Auth from "@/views/auth/index.vue";
+import Login from "@/views/auth/login/index.vue";
+import Register from "@/views/auth/register/index.vue";
+import { ElMessage } from "element-plus";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,16 +24,19 @@ const router = createRouter({
                     path: "/",
                     name: "home",
                     component: Home,
+                    meta: { title: "首页" },
                 },
                 {
                     path: "/chart",
                     name: "chart",
                     component: Chart,
+                    meta: { title: "购物车", requiresAuth: true },
                 },
                 {
                     path: "/me",
                     name: "me",
                     component: Me,
+                    meta: { title: "我的", requiresAuth: true },
                 },
                 {
                     path: "/chat",
@@ -41,7 +50,34 @@ const router = createRouter({
                 },
             ],
         },
+        {
+            path: "/auth",
+            name: "auth",
+            component: Auth,
+            children: [
+                {
+                    path: "/auth/login",
+                    name: "login",
+                    component: Login,
+                },
+                {
+                    path: "/auth/register",
+                    name: "register",
+                    component: Register,
+                },
+            ],
+        },
     ],
 });
+
+router.beforeEach((to, from, next) => {
+    const userStore = useUserStore();
+    if(to.meta.requiresAuth && !userStore.userInfo?.token) {
+        ElMessage.error("请先登录");
+        next('/auth/login');
+    } else {
+        next();
+    }
+})
 
 export default router;
