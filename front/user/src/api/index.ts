beeb74a1e383e51d5d8f58467e615a1b14277b7e
Guan612 <tiantony53@163.com>
2024-09-17 10:43:45 +0800

    添加api请求拦截器

---
+++ front/user/src/api/index.ts
@@ -0,0 +1,40 @@
+import { useUserStore } from "@/stores/userStore";
+import axios from "axios";
+import { ElMessage } from "element-plus";
+
+const http = axios.create({
+    baseURL: "http://localhost:3000",
+    timeout: 2000,
+});
+
+http.interceptors.request.use(
+    async (config) => {
+        const userStore = useUserStore();
+        const token = await userStore.userInfo?.token;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        console.log(error);
+        ElMessage.error(error.message);
+        return Promise.reject(error);
+    }
+);
+
+http.interceptors.response.use(
+    (response) => {
+        return response.data;
+    },
+    (error) => {
+        console.log(error);
+        ElMessage({
+            message: error.response.data.message,
+            type: "error",
+        });
+        return Promise.reject(error);
+    }
+);
+
+export default http;
