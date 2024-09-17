46fdfb12e48f25f4cad7f926d81afc586bf4a75d
Guan612 <tiantony53@163.com>
2024-09-17 10:44:34 +0800

    配合全局错误弹窗

--- front/user/src/main.ts
+++ front/user/src/main.ts
@@ -4,6 +4,7 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import "tailwindcss/tailwind.css";
 import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 import App from "./App.vue";
