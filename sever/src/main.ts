cc7540300420415057492949d37df1ee32236555
Guan612 <tiantony53@163.com>
2024-09-18 19:04:00 +0800

    增加静态访问代码

--- sever/src/main.ts
+++ sever/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
   app.enableCors(); // 允许跨域
   const config = new DocumentBuilder()
     .setTitle('鸭鸭点餐')
@@ -13,6 +16,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  //静态文件路径
+  app.useStaticAssets(configService.get('UPLOAD_PATH'), {
+    prefix: `/${configService.get('UPLOAD_PATH')}`,
+  });
 
   app.useGlobalPipes(new ValidationPipe()); // 全局验证
 
