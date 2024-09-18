053d851d9d735023af23721e4173f172239c0fd0
Guan612 <tiantony53@163.com>
2024-09-18 19:03:42 +0800

    增加上传测试代码

--- sever/src/upload/upload.controller.ts
+++ sever/src/upload/upload.controller.ts
@@ -1,8 +1,29 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FileSizeValidationPipe } from './pipe/file-validation.pipe';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @ApiTags('upload')
 @Controller('upload')
 export class UploadController {
-  
+  constructor(private readonly configService: ConfigService) {}
+  @Post('file')
+  @ApiOperation({ summary: '上传文件' })
+  @UseInterceptors(FileInterceptor('file', {}))
+  uploadFile(@UploadedFile(FileSizeValidationPipe) file) {
+    // 处理上传的文件
+    return {
+      message: 'File uploaded successfully',
+      fileName: file.filename,
+      path: file.path,
+    };
+  }
 }
