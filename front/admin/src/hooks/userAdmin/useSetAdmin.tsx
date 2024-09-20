bd7d868d5a48a07d83bd161b508cd75537864b90
Guan612 <tiantony53@163.com>
2024-09-20 11:27:28 +0800

    设置用户管理hook

--- front/admin/src/hooks/userAdmin/useSetAdmin.tsx
+++ front/admin/src/hooks/userAdmin/useSetAdmin.tsx
@@ -1,3 +1,18 @@
+import { useState } from "react"
+import { updateUserAPI } from "../../api/user"
+import { cost } from "../../dto/cost"
+
 const userSetAdmin =  () => {
+    const [user,setUser] = useState({})
+
+    const setAdmin = async (id:string) => {
+        const res = await updateUserAPI(id,{cost:2})
+        console.log(res)
+        setUser(res)
+    }
+
+    return {setAdmin}
     
 }
+
+export default userSetAdmin
