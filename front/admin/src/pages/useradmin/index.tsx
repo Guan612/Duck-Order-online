751e04a6f4bb8107b70fe73e31398aa44ebd3081
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-20 09:49:44 +0800

    增加了管理按钮

--- front/admin/src/pages/useradmin/index.tsx
+++ front/admin/src/pages/useradmin/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "antd";
 import useGetUserlist from "../../hooks/userAdmin/useGetUserlist";
 
 export default function UserAdmin() {
@@ -7,9 +8,17 @@ export default function UserAdmin() {
 			<h1 className="text-3xl font-bold m-4">用户管理</h1>
 			<ul>
 				{users.map((user) => (
-					<div className="bg-slate-400 m-2" key={user.id}>
-						<li>{user.loginId}</li>
-						<li>{getRoleName(user.cost)}</li>
+					<div className="flex justify-around-between items-center bg-slate-400 m-2">
+						<div className=" m-2" key={user.id}>
+							<li>{user.loginId}</li>
+							<li>{getRoleName(user.cost)}</li>
+						</div>
+						<div>
+							<Button type="primary">设置为管理员</Button>
+							<Button type="primary" danger>
+								删除用户
+							</Button>
+						</div>
 					</div>
 				))}
 			</ul>
