78f28bd2f45faa47bcb25a4abb67f7d20a226aee
Guan612 <tiantony53@163.com>
2024-09-20 11:26:23 +0800

    设置为管理员逻辑

--- front/admin/src/pages/useradmin/index.tsx
+++ front/admin/src/pages/useradmin/index.tsx
@@ -1,27 +1,38 @@
 import { Button } from "antd";
 import useGetUserlist from "../../hooks/userAdmin/useGetUserlist";
+import userSetAdmin from "../../hooks/userAdmin/useSetAdmin";
 
 export default function UserAdmin() {
-	const { users, getRoleName } = useGetUserlist();
-	return (
-		<>
-			<h1 className="text-3xl font-bold m-4">用户管理</h1>
-			<ul>
-				{users.map((user) => (
-					<div className="flex justify-around-between items-center bg-slate-400 m-2">
-						<div className=" m-2" key={user.id}>
-							<li>{user.loginId}</li>
-							<li>{getRoleName(user.cost)}</li>
-						</div>
-						<div>
-							<Button type="primary">设置为管理员</Button>
-							<Button type="primary" danger>
-								删除用户
-							</Button>
-						</div>
-					</div>
-				))}
-			</ul>
-		</>
-	);
+    const { users, getRoleName } = useGetUserlist();
+    const { setAdmin } = userSetAdmin();
+    return (
+        <>
+            <h1 className="text-3xl font-bold m-4">用户管理</h1>
+            <ul>
+                {users.map((user) => (
+                    <div className="flex justify-around-between items-center bg-slate-400 m-2">
+                        <div className="m-2 flex" key={user.id}>
+                            <div className="m-1">{user.loginId}</div>
+                            <div className="m-1">{getRoleName(user.cost)}</div>
+                        </div>
+                        <div className="flex m-1">
+                            <Button
+                                className="m-1"
+                                type="primary"
+                                onClick={() => setAdmin(user.id)}
+                            >
+                                设置为管理员
+                            </Button>
+                            <Button className="m-1" type="primary">
+                                重置密码
+                            </Button>
+                            <Button className="m-1" type="primary" danger>
+                                删除用户
+                            </Button>
+                        </div>
+                    </div>
+                ))}
+            </ul>
+        </>
+    );
 }
