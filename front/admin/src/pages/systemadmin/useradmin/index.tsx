0a9d88bc699ff492f5f8c7bab7bb5e702226ecd7
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-22 09:20:26 +0800

    调整位置

--- front/admin/src/pages/systemadmin/useradmin/index.tsx
+++ front/admin/src/pages/systemadmin/useradmin/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from "antd";
-import useGetUserlist from "../../hooks/userAdmin/useGetUserlist";
-import userSetAdmin from "../../hooks/userAdmin/useSetAdmin";
+import useGetUserlist from "../../../hooks/userAdmin/useGetUserlist";
+import userSetAdmin from "../../../hooks/userAdmin/useSetAdmin";
 
 export default function UserAdmin() {
     const { users, getRoleName } = useGetUserlist();
@@ -10,7 +10,7 @@ export default function UserAdmin() {
             <h1 className="text-3xl font-bold m-4">用户管理</h1>
             <ul>
                 {users.map((user) => (
-                    <div className="flex justify-around-between items-center bg-slate-400 m-2">
+                    <div className="flex justify-around-between items-center bg-white rounded-md m-2">
                         <div className="m-2 flex" key={user.id}>
                             <div className="m-1">{user.loginId}</div>
                             <div className="m-1">{getRoleName(user.cost)}</div>
