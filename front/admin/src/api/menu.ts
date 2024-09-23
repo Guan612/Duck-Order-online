41159d0f3b9e7e95db080d2941df21cfbffa7aa5
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-23 15:41:31 +0800

    调整类型

--- front/admin/src/api/menu.ts
+++ front/admin/src/api/menu.ts
@@ -4,6 +4,10 @@ export const getMenuListAPI = () => {
 	return http.get("/menu");
 };
 
-export const ChangeIsShellAPI = (id: string, isSell: number) => {
+export const ChangeIsShellAPI = (id: number, isSell: number) => {
 	return http.patch(`/menu/${id}`, {isSell: isSell});
 };
+
+export const searchMenuAPI = (menuName: string) => {
+	return http.get(`/menu/search/${menuName}`);
+}
