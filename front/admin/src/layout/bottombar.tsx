8310814a33f83437f3412daec2c19870cea9cdee
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-23 11:30:35 +0800

    底边栏

---
+++ front/admin/src/layout/bottombar.tsx
@@ -0,0 +1,9 @@
+export default function Bottombar() {
+  return(
+    <div>
+      <div className="flex justify-center items-center h-16 bg-gray-100">
+        <p className="text-sm text-gray-500">Copyright © 2022 Duck Order Online. All rights reserved.</p>
+      </div>
+    </div>
+  )
+}
