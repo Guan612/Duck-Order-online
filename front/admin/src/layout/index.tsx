d9db74a2e21769b810766aa0c76676f26239b07f
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-23 11:30:47 +0800

    Update index.tsx

--- front/admin/src/layout/index.tsx
+++ front/admin/src/layout/index.tsx
@@ -1,14 +1,21 @@
+import Bottombar from "./bottombar";
 import Sidebar from "./sidebar";
 import { Outlet } from "react-router-dom";
 export default function layout() {
 	return (
-		<div className="flex flex-row h-screen overflow-hidden">
-			<div className="hidden md:block md:basis-1/6 flex-col bg-red-300">
-				<Sidebar />
+		<div className="flex flex-col">
+			<div className="flex flex-row h-screen overflow-hidden">
+				<div className="hidden md:block md:basis-1/6 flex-col bg-red-300">
+					<Sidebar />
+				</div>
+				<div className="flex-1 flex-col md:basis-5/6 bg-blue-300 overflow-auto">
+					<Outlet />
+				</div>
 			</div>
-			<div className="flex-1 md:basis-5/6 bg-blue-300">
-				<Outlet />
-			</div>
+
+			{/* <div className="block md:hidden h-1/6">
+				<Bottombar />
+			</div> */}
 		</div>
 	);
 }
