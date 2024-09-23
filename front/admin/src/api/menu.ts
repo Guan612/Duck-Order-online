56a632de87611f3425a0df8286e557927f5a25d1
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-23 11:30:00 +0800

    增加菜单管理api

--- front/admin/src/api/menu.ts
+++ front/admin/src/api/menu.ts
@@ -1,5 +1,9 @@
 import http from "./index";
 
 export const getMenuListAPI = () => {
-    return http.get('/menu');
+	return http.get("/menu");
+};
+
+export const ChangeIsShellAPI = (id: string, isSell: number) => {
+	return http.patch(`/menu/${id}`, {isSell: isSell});
 };
