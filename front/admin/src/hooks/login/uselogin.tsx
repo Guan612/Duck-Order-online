dd65acb22d4148fc2140ced04c970bd789c06201
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-24 12:02:21 +0800

    添加提示登录弹窗

--- front/admin/src/hooks/login/uselogin.tsx
+++ front/admin/src/hooks/login/uselogin.tsx
@@ -1,16 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { adminLoginAPI } from "../../api/user";
 import userStore from "../../store/userstore";
-import userInfoDto from "../../dto/userInfo";
+import { message } from "antd";
+import { user } from "../../dto/user";
 
 const useLogin = () => {
 	const navigate = useNavigate(); //使用跳转函数
 	const { setUserInfo } = userStore();
-	const onFinish = async (values) => {
+	const onFinish = async (values: user) => {
 		try {
 			const { userInfo } = await adminLoginAPI(values);
 			setUserInfo(userInfo);
 			navigate("/");
+			message.success("欢迎你，" + userInfo.loginId);
 		} catch (error) {
 			console.error("Login failed:", error);
 		}
