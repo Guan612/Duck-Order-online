a67855aa9ecd74aff99b8a348fdbdedee44404ce
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-25 12:09:14 +0800

    添加加入购物车按钮

--- front/user/src/views/menu/componment/menuCard.vue
+++ front/user/src/views/menu/componment/menuCard.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref } from 'vue';
 import { menuType } from '@/dto/menuDto';
-
+import { ElButton } from 'element-plus';
 
 const props = defineProps({
+    foodId: Number,
     foodName: String,
     cardUrl: String,
     foodPrice: Number,
@@ -11,19 +12,31 @@ const props = defineProps({
     foodQuantity: Number
 })
 
+const foodId = ref(props.foodId);
+
 const getMenuType = (value) => {
     return menuType[value] || "未知食物";
 }
+
+const addToCart = () => {
+    
+}
+
 </script>
 
 <template>
     <div
-        class="flex flex-col bg-gradient-to-r from-transblue to-transpink max-w-sm m-2 rounded-lg shadow-md hover:shadow-lg transition duration-500 ease-in-out hover:scale-105">
+        class="flex flex-col bg-gradient-to-r from-transblue to-transpink max-w-sm m-2 rounded-lg shadow-md hover:shadow-lg transition duration-500 ease-in-out hover:scale-105 relative">
         <div class="font-bold">{{ foodName }}</div>
         <el-image :src="cardUrl" class="m-1 rounded-lg"></el-image>
-        <div class="text-base">
-            <p class="text-md text-blod">￥{{ foodPrice }}</p>
-            <p>类型: {{ getMenuType(foodType) }}</p>
+        <div class="flex flex-row text-base">
+            <div class="flex flex-col m-2">
+                <p class="text-md text-blod">￥{{ foodPrice }}</p>
+                <p>类型: {{ getMenuType(foodType) }}</p>
+            </div>
+            <div class="absolute bottom-2 right-2">
+                <ElButton type="primary" icon="plus" size="smaill" circle @click="addToCart"></ElButton>
+            </div>
         </div>
     </div>
 </template>
