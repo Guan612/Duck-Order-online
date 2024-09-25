07517fadc2005a9df8af6d433070e50935d1a9b4
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-25 10:25:14 +0800

    添加api

--- front/user/src/api/menu.ts
+++ front/user/src/api/menu.ts
@@ -1,5 +1,17 @@
 import http from "./index";
 
-export const getMenuList = () => {
+export const getMenuListAPI = () => {
 	return http.get("/menu");
+};
+
+export const searchMenuAPI = (menuName: string) => {
+	return http.get(`/menu/search/${menuName}`);
+};
+
+export const searchMenuByTypeAPI = (type: number[]) => {
+	return http.get("/menu/searchType/", { params: { menuType: type } });
+};
+
+export const getMenuByIdAPI = (id: string) => {
+	return http.patch(`/menu/${id}`);
 };
