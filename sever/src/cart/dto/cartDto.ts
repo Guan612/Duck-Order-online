44e53d3f054fa131cee6a97d604f089be8a71364
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-25 21:22:48 +0800

    Update cartDto.ts

--- sever/src/cart/dto/cartDto.ts
+++ sever/src/cart/dto/cartDto.ts
@@ -2,11 +2,5 @@ import { IsNotEmpty } from 'class-validator';
 
 export class CreateCartDto {
   @IsNotEmpty()
-  menuId: number;
-
-  @IsNotEmpty()
   userId: number;
-
-  @IsNotEmpty()
-  quantity: number;
 }
