16d4e6e4b1dc8a6add3d77fccf540b7549c38793
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-25 21:22:20 +0800

    Update cart.module.ts

--- sever/src/cart/cart.module.ts
+++ sever/src/cart/cart.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { OderlistService } from 'src/oderlist/oderlist.service';
 
 @Module({
   controllers: [CartController],
-  providers: [CartService, PrismaService],
+  providers: [CartService, PrismaService,OderlistService],
 })
 export class CartModule {}
