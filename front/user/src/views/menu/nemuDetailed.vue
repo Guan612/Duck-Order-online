3d184ae1538a9db052a35010309967fa199f6ff8
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-25 10:24:17 +0800

    详细菜单信息

---
+++ front/user/src/views/menu/nemuDetailed.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { getMenuByIdAPI } from '@/api/menu';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const detalParamValue = route.params.id
+
+const menuDetailed = ref({})
+
+const getDetaled = async (detalParamValue: string) => {
+    const res = await getMenuByIdAPI(detalParamValue)
+    menuDetailed.value = res
+}
+
+onMounted(() => {
+    if (detalParamValue) { // 检查参数是否存在
+        getDetaled(detalParamValue);
+    }
+})
+
+</script>
+
+<template>
+<div>详情信息{{menuDetailed.name}}</div>
+</template>
+
+<style scoped></style>
