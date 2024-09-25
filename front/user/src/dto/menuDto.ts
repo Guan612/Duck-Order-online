3aeb458bead02a7c4a09c0aee4a3330a36a93423
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-25 10:25:28 +0800

    添加菜单类型

---
+++ front/user/src/dto/menuDto.ts
@@ -0,0 +1,20 @@
+enum menuType {
+	"热菜" = 0,
+	"凉菜" = 1,
+	"主食" = 2,
+	"汤" = 3,
+	"饮品" = 4,
+}
+
+interface menu {
+	id: number;
+	name: string;
+	type: menuType;
+	price: number;
+	isSell: number;
+	discription?: string;
+	pictureUrl?: string;
+}
+
+export { menuType };
+export type { menu };
