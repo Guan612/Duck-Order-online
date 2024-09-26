f6d954e904b29e18a94930d0e7857b32765b3ce7
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-26 12:59:38 +0800

    Update foodcard.vue

--- front/user/src/views/home/component/foodcard.vue
+++ front/user/src/views/home/component/foodcard.vue
@@ -13,9 +13,10 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="bg-gradient-to-r from-transblue to-transpink max-w-sm m-2 rounded-lg shadow-md hover:shadow-lg transition duration-500 ease-in-out hover:scale-105">
+    <div
+        class="bg-gradient-to-r from-transblue to-transpink max-w-sm m-2 p-2 rounded-lg shadow-md hover:shadow-lg transition duration-500 ease-in-out hover:scale-105">
         <div class="font-bold">荒野机器人</div>
-        <el-image :src="cardUrl" class="m-1 rounded-lg"></el-image>
+        <el-image :src="cardUrl" class="rounded-lg"></el-image>
         <div class="text-gray-700 text-base">
             <p>Price: $10</p>
             <p>Quantity: 10</p>
