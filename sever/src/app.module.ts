f91828de72c93e3736abc83b23a98c69041360ff
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-26 13:02:10 +0800

    Update app.module.ts

--- sever/src/app.module.ts
+++ sever/src/app.module.ts
@@ -4,12 +4,20 @@ import { OrderModule } from './order/order.module';
 import { MenuModule } from './menu/menu.module';
 import { UploadModule } from './upload/upload.module';
 import { CartModule } from './cart/cart.module';
-import { ChatGateway } from './chat/chat.gateway';
-import { DeliveryGateway } from './delivery/delivery,getway';
+import { ChatModule } from './chat/chat.module';
+import { DeliveryModule } from './delivery/delivery.module';
 
 @Module({
-  imports: [UserModule, OrderModule, MenuModule, UploadModule, CartModule],
+  imports: [
+    UserModule,
+    OrderModule,
+    MenuModule,
+    UploadModule,
+    CartModule,
+    ChatModule,
+    DeliveryModule,
+  ],
   controllers: [],
-  providers: [ChatGateway,DeliveryGateway,],
+  providers: [],
 })
 export class AppModule {}
