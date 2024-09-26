9c50fb3b92066c3ddc881d4d4f52cd9f220d861d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-26 13:00:39 +0800

    Update menuCard.vue

--- front/user/src/views/menu/componment/menuCard.vue
+++ front/user/src/views/menu/componment/menuCard.vue
@@ -26,7 +26,7 @@ const addToCart = () => {
 
 <template>
     <div
-        class="flex flex-col bg-gradient-to-r from-transblue to-transpink max-w-sm m-2 rounded-lg shadow-md hover:shadow-lg transition duration-500 ease-in-out hover:scale-105 relative">
+        class="flex flex-col bg-gradient-to-r from-transblue to-transpink max-w-sm m-2 p-2 rounded-lg shadow-md hover:shadow-lg transition duration-500 ease-in-out hover:scale-105 relative">
         <div class="font-bold">{{ foodName }}</div>
         <el-image :src="cardUrl" class="m-1 rounded-lg"></el-image>
         <div class="flex flex-row text-base">
