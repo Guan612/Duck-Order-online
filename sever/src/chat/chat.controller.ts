aaa51913f4be8ac7aa6397c5319c283655390581
Guan612 <tiantony53@163.com>
2024-09-28 12:05:40 +0800

    获取聊天室列表

--- sever/src/chat/chat.controller.ts
+++ sever/src/chat/chat.controller.ts
@@ -1,6 +1,18 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/user/guards/jwt-user.guard';
+import { ChatService } from './chat.service';
 
 @Controller('chat')
 @ApiTags('chat')
-export class ChatController {}
+export class ChatController {
+    constructor(private readonly chatService: ChatService) {}
+
+
+    @Get('room')
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: '获取聊天房间' })
+    getRoom() {
+        return this.chatService.getRooms();
+    }
+}
