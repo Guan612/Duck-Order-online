58f5dd4360d2e0dec683c0b3c9ba38a6c0540db2
Guan612 <tiantony53@163.com>
2024-09-28 13:15:20 +0800

    Update chat.controller.ts

--- sever/src/chat/chat.controller.ts
+++ sever/src/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/user/guards/jwt-user.guard';
 import { ChatService } from './chat.service';
@@ -8,11 +8,17 @@ import { ChatService } from './chat.service';
 export class ChatController {
     constructor(private readonly chatService: ChatService) {}
 
-
     @Get('room')
     @UseGuards(JwtAuthGuard)
     @ApiOperation({ summary: '获取聊天房间' })
     getRoom() {
         return this.chatService.getRooms();
+    }
+
+    @Get('room/:id')
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: '通过id获取聊天房间' })
+    getRoomById(@Param('id') id: string) {
+        return this.chatService.getRoomById(+id);
     }
 }
