e0dd6c3c028b5aeac89794ad8bf8512cddc8dbdc
Guan612 <tiantony53@163.com>
2024-09-28 13:15:07 +0800

    增加将聊天加入聊天记录里面

--- sever/src/chat/chat.gateway.ts
+++ sever/src/chat/chat.gateway.ts
@@ -6,11 +6,11 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { connect } from 'http2';
 
 import { Server, Socket } from 'socket.io';
 import { JwtAuthGuard } from 'src/user/guards/jwt-user.guard';
 import { ChatService } from './chat.service';
+import { connectData } from './dto/chatDto';
 
 @WebSocketGateway(3001, { namespace: '/chat', cors: { origin: '*' } })
 export class ChatGateway {
@@ -27,18 +27,28 @@ export class ChatGateway {
     });
   }
 
-  //测试代码
-  @SubscribeMessage('chatTest')
-  async test(@MessageBody() Select, client: Socket) {
-    //console.log('chatTest', Select.userId);
-    const res = await this.chatService.chatTest(Select.userId);
-    this.server.emit('chatTest', { res }); // 只回复发送消息的客户端
+  @SubscribeMessage('onChat')
+  //@UseGuards(JwtAuthGuard)
+  async handleEvent(@MessageBody() connectData:connectData, client: Socket) {
+    //console.log('chat', payload.message.message);
+    const context = await this.chatService.addChatContext(connectData);
+    this.server.emit('chat', { message: connectData.content }); // 只回复发送消息的客户端
   }
 
-  @SubscribeMessage('chat')
-  //@UseGuards(JwtAuthGuard)
-  handleEvent(@MessageBody() payload, client: Socket) {
-    //console.log('chat', payload.message.message);
-    this.server.emit('chat', { message: payload.message.message }); // 只回复发送消息的客户端
+  @SubscribeMessage('disconnectChat')
+  handleDisconnect(client: Socket) {
+    //console.log('disconnect', client.id);
+    this.server.emit('disconnectChat', {
+      disconnectMeg: '断开连接',
+      clientId: client.id,
+    });
   }
+
+  //测试代码
+  // @SubscribeMessage('chatTest')
+  // async test(@MessageBody() Select, client: Socket) {
+  //   //console.log('chatTest', Select.userId);
+  //   const res = await this.chatService.chatTest(Select.userId);
+  //   this.server.emit('chatTest', { res }); // 只回复发送消息的客户端
+  // }
 }
--- sever/src/chat/chat.service.ts
+++ sever/src/chat/chat.service.ts
@@ -4,9 +4,10 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class ChatService {
   constructor(private readonly prisma: PrismaClient) {}
-  async chatTest(userId) {
-    const res = await this.prisma.user.findUnique({
-      where: { id: userId },
+  
+  async addChatContext(connectData){
+    const res = await this.prisma.message.create({
+      data: connectData
     });
     return res;
   }
@@ -15,4 +16,22 @@ export class ChatService {
     const res = await this.prisma.chatRoom.findMany();
     return res;
   }
+
+  async getRoomById(roomId){
+    const res = await this.prisma.chatRoom.findUnique({
+      where: { id: roomId },
+    });
+
+    return {
+      name: res.name,
+    };
+  }
+
+  async chatTest(userId) {
+    const res = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    return res;
+  }
+
 }
