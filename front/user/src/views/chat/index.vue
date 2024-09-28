59ca2cd926e9b8c2f370cccfadb40c01714f71d0
Guan612 <tiantony53@163.com>
2024-09-28 13:13:26 +0800

    Update index.vue

--- front/user/src/views/chat/index.vue
+++ front/user/src/views/chat/index.vue
@@ -14,7 +14,7 @@ onMounted(()=>{
 </script>
 <template>
     <div class="flex flex-col m-2 p-2">
-        <div class="text-2xl font-bold mb-2">聊天室列表</div>
+        <div class="text-2xl font-bold mb-2">聊天</div>
         <div v-for="item in roomList" :key="item.id" class="w-full bg-transblue p-2 border border-gray-300 rounded-md mb-2 cursor-pointer hover:bg-transpink" @click="$router.push(`/chat/${item.id}`)">
             {{item.name}}
         </div>
