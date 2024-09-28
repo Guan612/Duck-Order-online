c3e7befeb95769680c2b40d0a42f0cb5e8e3564c
Guan612 <tiantony53@163.com>
2024-09-28 12:04:29 +0800

    修改聊天列表

--- front/user/src/views/chat/index.vue
+++ front/user/src/views/chat/index.vue
@@ -1,7 +1,22 @@
 <script lang="ts" setup>
+import {getRoomListAPI} from '@/api/chat'
+import { onMounted, ref } from 'vue';
+const roomList = ref([])
+
+const getRoomList = async ()=>{
+    const res = await getRoomListAPI()
+    roomList.value = res
+}
+
+onMounted(()=>{
+    getRoomList()
+})
 </script>
 <template>
     <div class="flex flex-col m-2 p-2">
-        <div @click="$router.push('/chat/1')">点我进入聊天测试</div>
+        <div class="text-2xl font-bold mb-2">聊天室列表</div>
+        <div v-for="item in roomList" :key="item.id" class="w-full bg-transblue p-2 border border-gray-300 rounded-md mb-2 cursor-pointer hover:bg-transpink" @click="$router.push(`/chat/${item.id}`)">
+            {{item.name}}
+        </div>
     </div>
 </template>
