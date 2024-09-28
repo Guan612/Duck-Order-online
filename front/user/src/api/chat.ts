77a0f7780e1007d2afca0a5b07cd7666f6d64ba5
Guan612 <tiantony53@163.com>
2024-09-28 13:12:57 +0800

    新增加获取聊天名字

--- front/user/src/api/chat.ts
+++ front/user/src/api/chat.ts
@@ -3,3 +3,7 @@ import http from ".";
 export const getRoomListAPI = () => {
   return http.get("/chat/room");
 }
+
+export const getRoomByIdAPI = (id: string) => {
+	return http.get(`/chat/room/${id}`);
+};
