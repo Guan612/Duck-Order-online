fa5e334356d528df2c380551fdca9350362240f7
Guan612 <tiantony53@163.com>
2024-09-29 17:34:55 +0800

    用户端下单按钮测试

--- front/user/src/views/cart/index.vue
+++ front/user/src/views/cart/index.vue
@@ -1,7 +1,22 @@
-<script setup>
+<script setup lang="ts">
+import { io } from "socket.io-client";
 
+const socket = io('http://localhost:3001/order')
+socket.on("haveNewOder", () => { })
+
+
+const haveNewOder = async () => {
+    socket.emit("haveNewOder", {
+        message: "您有新的订单，请及时处理"
+    })
+    console.log('下单了')
+}
 </script>
 
 <template>
-    <div>cart</div>
+    <div class="flex flex-col">
+        <div>购物车页面</div>
+        <ElButton @click="haveNewOder">下单测试按钮</ElButton>
+    </div>
+
 </template>
