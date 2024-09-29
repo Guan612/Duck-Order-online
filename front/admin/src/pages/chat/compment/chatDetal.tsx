7912635d1008bab663b0ae343ad2d75f9c96f5ec
Guan612 <tiantony53@163.com>
2024-09-29 11:54:30 +0800

    Update chatDetal.tsx

--- front/admin/src/pages/chat/compment/chatDetal.tsx
+++ front/admin/src/pages/chat/compment/chatDetal.tsx
@@ -1,5 +1,9 @@
 export default function ChatDetal() {
     return (
-        <div>lia</div>
+        <div className="flex flex-col h-full">
+            <div className="flex-1/8 bg-red-200">ches</div>
+            <div className="flex-1 overflow-y-auto">2111</div>
+            <div className="flex-1/8 bg-red-200">发送区域</div>
+        </div>
     )
 }
