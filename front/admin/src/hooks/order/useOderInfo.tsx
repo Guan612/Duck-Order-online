339c9998caddffa3ba3793780e773aa362fe22b3
Guan612 <tiantony53@163.com>
2024-09-29 17:27:32 +0800

    用户下单语音提示

---
+++ front/admin/src/hooks/order/useOderInfo.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import useSocket from "../../api/socket";
+import { message } from "antd";
+export default function useOderInfo() {
+    const socket = useSocket("order")
+    const [orderMessage, setOrderMessage] = useState([])
+
+    const speak = (text: string) => {
+        const utterance = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(utterance);
+    };
+
+    const testOrder = () => {
+        socket.emit('haveNewOder', {
+            message: "admin已连接"
+        })
+    }
+
+    useEffect(() => {
+        socket.on('haveNewOder', (data) => {
+            if (data) {
+                speak(data.message);
+                message.info(data.message);
+                setOrderMessage(data.message);
+            }
+        });
+
+        // 清理连接
+        return () => {
+            socket.off('haveNewOder');
+            socket.disconnect();
+        };
+    }, [socket]);
+
+    return { orderMessage, testOrder }
+}
