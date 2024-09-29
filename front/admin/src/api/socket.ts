63c7dc22ac6cf224b38384028a7d23aeae5ca26d
Guan612 <tiantony53@163.com>
2024-09-29 17:25:18 +0800

    添加socket基础链接url

--- front/admin/src/api/socket.ts
+++ front/admin/src/api/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 export default function useScoket(path: string) {
-    const socket = io(`http://localhost:3001/${path}`)
+    const socket =  io(`http://localhost:3001/${path}`)
     return socket
 }
