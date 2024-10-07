6bdf230e588b33f11c86eeac4008b580fc2de56d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-07 16:40:10 +0800

    添加系统通知功能

--- front/admin/src/hooks/order/useOderInfo.tsx
+++ front/admin/src/hooks/order/useOderInfo.tsx
@@ -1,37 +1,61 @@
 import { useEffect, useState } from "react";
 import useSocket from "../../api/socket";
 import { message } from "antd";
+import OrderInfo from "../../pages/order/oderInfo";
 export default function useOderInfo() {
-  const socket = useSocket("order");
-  const [orderMessage, setOrderMessage] = useState([]);
+	const socket = useSocket("order");
+	const [orderMessage, setOrderMessage] = useState([]);
+	const [orderList, setOrderList] = useState([]);
 
-  const speak = (text: string) => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(utterance);
-  };
+	//语音播报功能
+	const speak = (text: string) => {
+		const utterance = new SpeechSynthesisUtterance(text);
+		window.speechSynthesis.speak(utterance);
+	};
 
-  const testOrder = () => {
-    socket.emit("haveNewOder", {
-      message: "张佳宁是原批",
-    });
-  };
+	// 系统通知功能
+	const showNotification = (message) => {
+		// 检查浏览器是否支持通知
+		if ("Notification" in window) {
+			// 请求通知权限
+			if (Notification.permission === "granted") {
+				// 显示通知
+				new Notification("新订单提醒", { body: message });
+			} else if (Notification.permission !== "denied") {
+				// 请求权限
+				Notification.requestPermission().then((permission) => {
+					if (permission === "granted") {
+						new Notification("新订单提醒", { body: message });
+					}
+				});
+			}
+		}
+	};
 
-  useEffect(() => {
-    const handleNewOrder = (data) => {
-      if (data) {
-        speak(data.message);
-        message.info(data.message);
-        setOrderMessage(data.message);
-      }
-    };
+	// const testOrder = () => {
+	// 	socket.emit("haveNewOder", {
+	// 		message: "张佳宁是原批",
+	// 	});
+	// };
 
-    socket.on("haveNewOder", handleNewOrder);
+	useEffect(() => {
+		const handleNewOrder = (data) => {
+			if (data) {
+				speak(data.message);
+				showNotification(data.message);
+				message.info(data.message);
+				setOrderMessage(data.message);
+				setOrderList(data.orderInfo);
+			}
+		};
 
-    // 清理连接
-    return () => {
-      socket.off("haveNewOder", handleNewOrder);
-    };
-  }, [socket]);
+		socket.on("haveNewOder", handleNewOrder);
 
-  return { orderMessage, testOrder };
+		// 清理连接
+		return () => {
+			socket.off("haveNewOder", handleNewOrder);
+		};
+	}, [socket]);
+
+	return { orderMessage, orderList };
 }
