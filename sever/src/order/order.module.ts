ca790a24e0a86adfb6528cc1c89699073922532e
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-08 14:11:21 +0800

    调整名字

--- sever/src/order/order.module.ts
+++ sever/src/order/order.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { OderlistService } from 'src/oderlist/oderlist.service';
+import { OderlistService } from 'src/orderlist/orderlist.service';
 import { OrderGateway } from './order.gateway';
 
 @Module({
