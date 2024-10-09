a088b6d5acb72c74956783670aca00c62e2722b0
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-09 16:40:02 +0800

    调整订单管理布局

--- front/admin/src/pages/order/index.tsx
+++ front/admin/src/pages/order/index.tsx
@@ -13,45 +13,51 @@ export default function Order() {
 		orderSelectChange,
 	} = useOrder();
 	return (
-		<div className="flex flex-col h-screen">
-			<div className="flex flex-row items-center justify-center p-2 font-bold text-2xl">
-				订单管理
+		<div className="flex flex-col h-full">
+			<div className="h-1/8 flex flex-col">
+				<div className="flex flex-row items-center justify-center p-2 font-bold text-2xl">
+					订单管理
+				</div>
+				<div className="flex flex-row items-center justify-center p-2">
+					<Select
+						className="w-96 h-8"
+						mode="multiple"
+						allowClear
+						placeholder="查询订单类型"
+						defaultValue={[]}
+						onChange={orderSelectChange}
+						options={orderOptions}
+					/>
+				</div>
 			</div>
-			<div className="flex flex-row items-center justify-center p-2">
-				<Select
-					className="w-96 h-8"
-					mode="multiple"
-					allowClear
-					placeholder="查询订单类型"
-					defaultValue={[]}
-					onChange={orderSelectChange}
-					options={orderOptions}
-				/>
-			</div>
-			{orderList.map((item) => (
-				<div
-					className="flex justify-between items-center p-2 border-b-2 min-h-16 overflow-y-auto"
-					key={item.id}
-				>
-					<div>
-						<div className="mx-1">订单编号：{item.id}</div>
-						<div className="mx-1">订单金额：{item.totalPrice}</div>
-						<div className="mx-1 font-bold">
-							订单状态：{getOrderStatus(item.orderStatus)}
+			<div className="m-1 p-2 overflow-y-auto">
+				{orderList.map((item) => (
+					<div
+						className="flex justify-between items-center p-2 border-b-2 min-h-16 "
+						key={item.id}
+					>
+						<div>
+							<div className="mx-1">订单编号：{item.id}</div>
+							<div className="mx-1">
+								订单金额：{item.totalPrice}
+							</div>
+							<div className="mx-1 font-bold">
+								订单状态：{getOrderStatus(item.orderStatus)}
+							</div>
+						</div>
+						<div>
+							<Button
+								className="mx-1"
+								type="primary"
+								onClick={() => showOrderModal(item.id)}
+							>
+								订单详情
+							</Button>
+							<Button>订单修改</Button>
 						</div>
 					</div>
-					<div>
-						<Button
-							className="mx-1"
-							type="primary"
-							onClick={() => showOrderModal(item.id)}
-						>
-							订单详情
-						</Button>
-						<Button>订单修改</Button>
-					</div>
-				</div>
-			))}
+				))}
+			</div>
 			<Modal
 				title="订单详情"
 				open={isOrderModalOpen}
