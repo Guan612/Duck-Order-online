4938ece548897778e78dc862e04d0aab1cf1185a
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-09 12:36:05 +0800

    新增cartlist相关service

--- sever/src/cartlist/cartlist.service.ts
+++ sever/src/cartlist/cartlist.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CartlistService {}
+export class CartlistService {
+  constructor(private prisma: PrismaService) {}
+  addCartList(addCartListDto) {
+    const {
+      cartId,
+      menuId,
+      quantity,
+    }: {
+      cartId: number;
+      menuId: number;
+      quantity?: number;
+    } = addCartListDto;
+    return this.prisma.cartList.create({
+      data: {
+        cartId: cartId,
+        menuId: menuId,
+        quantity: quantity || 1,
+      },
+    });
+  }
+
+  async getCartList(cartId: number) {
+    const res = await this.prisma.cartList.findMany({
+      where: {
+        cartId: cartId,
+      },
+      include: {
+        menu: {
+          select: {
+            name: true,
+            price: true,
+          },
+        },
+      },
+    });
+    return res;
+  }
+}
