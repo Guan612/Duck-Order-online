98cb70b676df9c1a453fb925b74849a7e09e240d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-09 17:19:02 +0800

    添加支付流程

--- front/user/src/views/order/index.vue
+++ front/user/src/views/order/index.vue
@@ -5,15 +5,23 @@ import { createOrderAPI, getOrderDetaiLListAPI } from '@/api/order'
 import useScoket from '@/api/socket';
 
 const orderList = ref([]);
-const socket = useScoket('order')
-
+const socket = useScoket('order');
 const route = useRoute();
+const orderDialogVisible = ref(false);
 
 socket.on("haveNewOder", () => { })
 const haveNewOder = async () => {
     socket.emit("haveNewOder", {
         message: "您有新的订单，请及时处理",
         orderList: orderList.value
+    })
+    orderDialogVisible.value = false
+}
+
+const exitPayment = async () => {
+    socket.emit("exitPayment", {
+        message: "用户取消支付",
+        orderId: route.params.id
     })
 }
 
@@ -44,8 +52,19 @@ onMounted(() => {
         <div class="sticky bottom-0 bg-white p-4 border-t flex flex-col">
             <div class="flex justify-end">
                 <el-button type="danger" @click="$router.back()">取消</el-button>
-                <el-button type="primary" @click="haveNewOder">确认</el-button>
+                <el-button type="primary" @click="orderDialogVisible = true">确认</el-button>
             </div>
         </div>
+        <el-dialog v-model="orderDialogVisible" title="支付" width="500">
+            <span>请您确认金额</span>
+            <template #footer>
+                <div class="dialog-footer">
+                    <el-button @click="orderDialogVisible = false">取消支付</el-button>
+                    <el-button type="primary" @click="haveNewOder">
+                        支付完成
+                    </el-button>
+                </div>
+            </template>
+        </el-dialog>
     </div>
 </template>
