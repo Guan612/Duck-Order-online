1e0d1d2d68f0666d47885d6051ad453ffb5cc9df
Guan612 <tiantony53@163.com>
2024-10-17 11:11:04 +0800

    签到加分用户端

--- front/user/src/api/userbalance.ts
+++ front/user/src/api/userbalance.ts
@@ -3,3 +3,7 @@ import http from ".";
 export const getUserBalanceAPI = () => {
   return http.get(`/userbalance/`);
 }
+
+export const activityActiveAPI = (id:number,balance:number) => {
+  return http.post(`/userbalance/activity/${id}`, {balance:balance})
+}
--- front/user/src/views/me/index.vue
+++ front/user/src/views/me/index.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { getUserBalanceAPI } from '@/api/userbalance';
+import { getUserBalanceAPI,activityActiveAPI } from '@/api/userbalance';
+import { ElMessage } from 'element-plus';
 
-const balance = ref(0);
+const balanceInfo = ref({});
 const getBalance = async () => {
     const res = await getUserBalanceAPI();
     if (res) {
-        balance.value = res.balance
+        balanceInfo.value = res
+    }
+}
+
+const activity = ref(0);
+const activityState = ref('未签到');
+const activityChange = async (balanceInfo)=>{
+    activity.value += 1;
+    if(activity.value%2 == 0){
+        ref.value = 0;
+        ElMessage.error('未签到')
+        activityState.value = '未签到'
+    } else { 
+        const res =  await activityActiveAPI(balanceInfo.id,balanceInfo.balance)
+        if(res){
+            ElMessage.success('签到成功')
+            activityState.value = '已签到'
+        }
     }
 }
 
@@ -20,7 +38,10 @@ onMounted(()=>{
         <h1 class="text-2xl font-bold text-center m-2 p-2">个人中心</h1>
         <div>
             <div class="">我的订单</div>
-            <div class="">我的余额{{balance}}</div>
+            <div class="">我的余额{{balanceInfo.balance}}</div>
+            <div>
+                <el-button type="primary" @click="activityChange(balanceInfo)">{{activityState}}</el-button>
+            </div>
         </div>
     </div>
 </template>
