48f24b9296b9ac2acf6630aa8500a610ca99e55c
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-18 17:03:47 +0800

    完成文章测试页面

--- front/user/src/views/article/index.vue
+++ front/user/src/views/article/index.vue
@@ -4,9 +4,14 @@ import articleDetail from './component/articleDetail.vue';
 
 <template>
   <div class="flex flex-col">
-    <h1 class="text-2xl text-center font-bold mb-4">文章</h1>
+    <h1 class="text-2xl text-center font-bold mb-4 justify-center">文章列表</h1>
     <div class="flex flex-col">
-      <div @click="$router.push('/article/1')" class="m-2">文章1</div>
+      <div @click="$router.push('/article/1')" class="m-2 h-10 rounded-xl bg-transblue items-center justify-center">
+        <span class="text-xl font-bold">文章1</span>
+      </div>
+      <div @click="$router.push('/article/2')" class="m-2 h-10 rounded-xl bg-transblue items-center justify-center">
+        <span class="text-xl font-bold">文章2</span>
+      </div>
     </div>
   </div>
 </template>
