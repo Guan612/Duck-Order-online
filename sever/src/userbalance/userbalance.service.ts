68258097b2ac18b1eb34c51e30c2c663cba8a894
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-18 17:04:34 +0800

    更新排名返回方式

--- sever/src/userbalance/userbalance.service.ts
+++ sever/src/userbalance/userbalance.service.ts
@@ -1,77 +1,87 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserBalanceService {
-    constructor(private prisma: PrismaService) {}
-    async create(userId: number) {
-        const res = await this.prisma.userBalance.create({
-            data:{
-                userId: userId,
-            }
-        })
-        return res;
-    }
+  constructor(private prisma: PrismaService) {}
+  async create(userId: number) {
+    const res = await this.prisma.userBalance.create({
+      data: {
+        userId: userId,
+      },
+    });
+    return res;
+  }
 
-    async findUserBalance(userId: number) {
-        const res = await this.prisma.userBalance.findFirst({
-            where: {
-                userId: userId,
-            }
-        })
-        return res;
-    }
+  async findUserBalance(userId: number) {
+    const res = await this.prisma.userBalance.findFirst({
+      where: {
+        userId: userId,
+      },
+    });
+    return res;
+  }
 
-    async update(id: number, balance: number) {
-        const res = await this.prisma.userBalance.update({
-          where: {
-            id: id,  // 使用传入的 id 变量
+  async update(id: number, balance: number) {
+    const res = await this.prisma.userBalance.update({
+      where: {
+        id: id, // 使用传入的 id 变量
+      },
+      data: {
+        balance: balance, // 直接更新 balance 值
+      },
+    });
+    return res;
+  }
+
+  async findAll() {
+    const res = await this.prisma.userBalance.findMany({
+      include: {
+        user: {
+          select: {
+            loginId: true,
           },
-          data: {
-            balance: balance,  // 直接更新 balance 值
+        },
+      },
+    });
+
+    return res.map(({ user, ...balanceData }) => ({
+      ...balanceData,
+      loginId: user.loginId,
+    }));
+  }
+
+  async addActive(id: number, balance: number) {
+    const res = await this.prisma.userBalance.update({
+      where: {
+        id: id,
+      },
+      data: {
+        balance: balance,
+      },
+    });
+
+    return res;
+  }
+
+  async upBalance() {
+    const res = await this.prisma.userBalance.findMany({
+      orderBy: {
+        balance: 'desc',
+      },
+      take: 10,
+      include: {
+        user: {
+          select: {
+            loginId: true,
           },
-        });
-        return res;
-      }
+        },
+      },
+    });
 
-    async findAll() {
-        const res = await this.prisma.userBalance.findMany({
-            include:{
-                user:{
-                    select:{
-                        loginId: true,
-                    }
-                }
-            }
-        })
-
-        return res.map(({ user, ...balanceData }) => ({
-            ...balanceData,
-            loginId: user.loginId,
-        }));
-    }
-
-    async addActive(id: number,balance: number) {
-        const res = await this.prisma.userBalance.update({
-            where:{
-                id:id,
-            },
-            data:{
-                balance:balance
-            }
-        })
-
-        return res;
-    }
-
-    async upBalance(){
-        const res = await this.prisma.userBalance.findMany({
-            orderBy:{
-                balance:'desc'
-            },
-            take:10,
-        });
-
-        return res;
-    }
+    return res.map(({ user, ...balanceData }) => ({
+      ...balanceData,
+      loginId: user.loginId,
+    }));
+  }
 }
