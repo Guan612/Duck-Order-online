5ab6279218a990b75c17b6d27bed59f6c40077a1
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-23 15:43:04 +0800

    搞定单文件按照扩展名上传

--- sever/src/upload/upload.controller.ts
+++ sever/src/upload/upload.controller.ts
@@ -9,7 +9,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FileSizeValidationPipe } from './pipe/file-validation.pipe';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
+import { extname, join } from 'path';
 import { ConfigService } from '@nestjs/config';
 
 @ApiTags('upload')
@@ -22,14 +22,20 @@ export class UploadController {
     FileInterceptor('file', {
       storage: diskStorage({
         destination: (req, file, cb) => {
-          // 获取文件后缀
           const fileExt = extname(file.originalname).toLowerCase();
 
+          // 从 .env 文件中读取允许的文件类型
+          const allowedPicTypes = process.env.ALLOWED_PIC_TYPES.split(',');
+          const allowedDocTypes = process.env.ALLOWED_DOC_TYPES.split(',');
+          const allowedVideoTypes = process.env.ALLOWED_VIDEO_TYPES.split(',');
+
           // 根据文件后缀选择不同的目录
-          if (fileExt === '.jpg' || fileExt === '.png') {
+          if (allowedPicTypes.includes(fileExt)) {
             cb(null, process.env.PIC_PATH || './public/images');
-          } else if (fileExt === '.pdf' || fileExt === '.txt') {
-            cb(null, process.env.PDF_PATH || './public/pdfs');
+          } else if (allowedDocTypes.includes(fileExt)) {
+            cb(null, process.env.DOC_PATH || './public/pdfs');
+          } else if (allowedVideoTypes.includes(fileExt)) {
+            cb(null, process.env.VIDEO_PATH || './public/videos');
           } else {
             // 不支持的文件类型
             cb(new BadRequestException('不支持的文件类型'), null);
@@ -47,12 +53,18 @@ export class UploadController {
   )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
-      throw new BadRequestException('File upload failed');
+      throw new BadRequestException('文件上传失败');
     }
-    // 返回文件路径
+
+    // 拼接文件路径
+    const filePath = join(file.destination, file.filename);
+    const baseUrl = this.configService.get<string>('BASE_URL') || 'http://localhost:3000/'; // 你可以设置自己的基础 URL
+    const relativeUrl = filePath.replace(/\\/g, '/'); // 将反斜杠替换为斜杠
+    const fullUrl = `${baseUrl}${relativeUrl}`;
+
     return {
-      message: 'File uploaded successfully',
-      filePath: `/uploads/${file.filename}`,
+      message: '文件上传成功',
+      filePath: fullUrl, // 返回完整路径
     };
   }
 }
