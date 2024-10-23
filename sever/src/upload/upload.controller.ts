7232777b43d71de22f872c37065b7967b8d64822
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-23 11:57:01 +0800

    修改文件上传代码

--- sever/src/upload/upload.controller.ts
+++ sever/src/upload/upload.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -17,13 +18,41 @@ export class UploadController {
   constructor(private readonly configService: ConfigService) {}
   @Post('file')
   @ApiOperation({ summary: '上传文件' })
-  @UseInterceptors(FileInterceptor('file', {}))
-  uploadFile(@UploadedFile(FileSizeValidationPipe) file) {
-    // 处理上传的文件
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          // 获取文件后缀
+          const fileExt = extname(file.originalname).toLowerCase();
+
+          // 根据文件后缀选择不同的目录
+          if (fileExt === '.jpg' || fileExt === '.png') {
+            cb(null, process.env.PIC_PATH || './public/images');
+          } else if (fileExt === '.pdf' || fileExt === '.txt') {
+            cb(null, process.env.PDF_PATH || './public/pdfs');
+          } else {
+            // 不支持的文件类型
+            cb(new BadRequestException('不支持的文件类型'), null);
+          }
+        },
+        filename: (req, file, cb) => {
+          // 生成唯一文件名，防止文件名冲突
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileExt = extname(file.originalname);
+          cb(null, `${uniqueSuffix}${fileExt}`);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('File upload failed');
+    }
+    // 返回文件路径
     return {
       message: 'File uploaded successfully',
-      fileName: file.filename,
-      path: file.path,
+      filePath: `/uploads/${file.filename}`,
     };
   }
 }
--- sever/src/upload/upload.module.ts
+++ sever/src/upload/upload.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    MulterModule.register({
+      dest: process.env.UPLOAD_PATH || 'public', // 默认文件上传目录
+    }),
+  ],
   controllers: [UploadController],
   providers: [],
 })
