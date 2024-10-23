0a3c8ff509a77d5af96a81f7cfac76651528e2a0
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-23 10:18:11 +0800

    优化我的订单布局

--- front/user/src/views/me/component/myorder.vue
+++ front/user/src/views/me/component/myorder.vue
@@ -31,26 +31,29 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col h-full">
         <div class="flex flex-row items-center">
             <ElButton type="primary" class="m-2" round icon="ArrowLeft" @click="$router.push('/me')"></ElButton>
             <div class="text-2xl font-bold text-center m-2 p-2">我的订单</div>
         </div>
 
         <!-- 遍历订单列表 -->
-        <div v-for="item in myorderList" :key="item.id"
-            class="flex flex-row items-center justify-between m-2 p-2 bg-transblue rounded-lg">
-            <div class="flex flex-col m-2">
-                <div>订单编号：{{ item.id }}</div>
-                <div :class="{ 'text-green-500': item.orderStatus === 1, 'text-red-600': item.orderStatus === 2 }">
-                    订单状态：{{ getOrderStatus(item.orderStatus) }}</div>
-                <div>订单金额：{{ item.totalPrice }}元</div>
-            </div>
-            <div class="flex flex-col m-2">
-                <!-- 点击按钮时调用 openDialog 方法，并传递订单的菜单列表 -->
-                <ElButton type="primary" round @click="openDialog(item.menuList)">详情</ElButton>
+        <div class="overflow-y-auto h-full">
+            <div v-for="item in myorderList" :key="item.id"
+                class="flex flex-row items-center justify-between m-2 p-2 bg-transblue rounded-lg">
+                <div class="flex flex-col m-2">
+                    <div>订单编号：{{ item.id }}</div>
+                    <div :class="{ 'text-green-500': item.orderStatus === 1, 'text-red-600': item.orderStatus === 2 }">
+                        订单状态：{{ getOrderStatus(item.orderStatus) }}</div>
+                    <div>订单金额：{{ item.totalPrice }}元</div>
+                </div>
+                <div class="flex flex-col m-2">
+                    <!-- 点击按钮时调用 openDialog 方法，并传递订单的菜单列表 -->
+                    <ElButton type="primary" round @click="openDialog(item.menuList)">详情</ElButton>
+                </div>
             </div>
         </div>
+
 
         <!-- 订单详情对话框 -->
         <el-dialog v-model="dialogVisible" title="订单详情">
