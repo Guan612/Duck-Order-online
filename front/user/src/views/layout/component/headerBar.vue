a84e3dd47bd5ee80737e09ca2fab88b131b535d5
Guan612 <tiantony53@163.com>
2024-10-24 19:59:56 +0800

    添加了用户登陆显示名字

--- front/user/src/views/layout/component/headerBar.vue
+++ front/user/src/views/layout/component/headerBar.vue
@@ -1,7 +1,16 @@
 <script setup>
+import {useUserStore} from '@/stores/userstore'
+import { ref } from 'vue';
+const {userInfo} = useUserStore()
+const userName = ref('')
+if (userInfo.loginId) {
+	userName.value = userInfo.loginId
+} else {
+	userName.value = '未登录'
+}
 </script>
 <template>
 	<div class="bg-gradient-to-r from-transblue to-transpink h-16 flex">
-
+		<div class="flex-1 flex justify-end items-center text-2xl font-bold m-2">{{userName}}</div>
 	</div>
 </template>
