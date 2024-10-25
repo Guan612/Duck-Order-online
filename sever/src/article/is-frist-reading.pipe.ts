2a8ba7d6e748c1b963014db9bbaff48b2c3ab47f
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-25 19:14:49 +0800

    是否第一次阅读管道

---
+++ sever/src/article/is-frist-reading.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  PipeTransform,
+  Injectable,
+  HttpException,
+  ArgumentMetadata,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class IsFirstReadingPipe implements PipeTransform {
+  transform(value: any) {
+    
+
+
+    return value;
+  }
+}
